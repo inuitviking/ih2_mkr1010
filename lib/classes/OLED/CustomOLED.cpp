5d137b639760b2164574490f35070a202eb67827
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-05-02 16:02:56 +0200

    Added OLED display

---
+++ lib/classes/OLED/CustomOLED.cpp
@@ -0,0 +1,5 @@
+//
+// Created by angus on 5/2/22.
+//
+
+#include "CustomOLED.h"
---
+++ lib/classes/OLED/CustomOLED.h
@@ -0,0 +1,14 @@
+//
+// Created by angus on 5/2/22.
+//
+
+#ifndef IH2_MKR1010_CUSTOMOLED_H
+#define IH2_MKR1010_CUSTOMOLED_H
+
+
+class CustomOLED {
+
+};
+
+
+#endif //IH2_MKR1010_CUSTOMOLED_H
